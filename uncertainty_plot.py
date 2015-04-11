a884c351785c0a92f766c8fa88fee437048f6712
zpace <zpace21@gmail.com>
2015-04-11 15:11:00 -0500

    added masking capability

--- uncertainty_plot.py
+++ uncertainty_plot.py
@@ -8,16 +8,19 @@ class uncertainty_plot(object):
 
 	Arguments:
 		- data: (n x m) array of measured values
-		- err: (n x m) array of unceertainties (direct correspondence to 
+		- err: (n x m) array of uncertainties (direct correspondence to 
 			position in `data` array)
+		- mask: (n x m) array of bools; True in a given position sets 
+			that position as 'bad'
 		- n: integer, essentially the number of pages in the flipbook
 		- **kwargs: passed to imshow, like `cmap` and bad data values
 	'''
-	def __init__(self, data, err, n = 100, title = None, **kwargs):
+	def __init__(self, data, err, mask, n = 100, title = None, **kwargs):
 
 		import numpy as np
 		import matplotlib.pyplot as plt
-		from matplotlib import animation
+		from matplotlib import animation, cm
+		import copy
 
 		plt.ioff()
 
@@ -28,22 +31,30 @@ class uncertainty_plot(object):
 		fig = plt.figure(figsize = self.figsize)
 
 		err_m = err[np.newaxis, :] * np.random.randn(n, data.shape[0], data.shape[1])
-		display_data = np.tile(data, (n, 1, 1)) + err_m
+		display_data = data[np.newaxis, :] + err_m
+
+		#set up the cmap
+		self.cmap = copy.copy(cm.cubehelix)
+		self.cmap.set_bad('#545454', alpha = 1.)
 
 		v_min = np.min(display_data)
 		v_max = np.max(display_data)
-		im = plt.imshow(data + np.random.randn(data.shape[0], data.shape[1]), interpolation = 'none', vmin = v_min, vmax = v_max, cmap = 'cubehelix', **kwargs)
+
+		im = plt.imshow(np.ma.array(display_data[0], mask = mask), 
+			interpolation = 'none', vmin = v_min, vmax = v_max, cmap = self.cmap, 
+			origin = 'lower', **kwargs)
+
 		plt.colorbar(im, shrink = 0.8, label = self.barlabel)
 		if title != None: plt.title(title, size = 16)
 
 		# initialization function: plot the background of each frame
 		def init():
-			im.set_data(display_data[0])
+			im.set_data(np.ma.array(display_data[0], mask = mask))
 			return [im]
 
 		# animation function.  This is called sequentially
 		def animate(i):
-			im.set_array(display_data[i])
+			im.set_array(np.ma.array(display_data[i], mask = mask))
 			return [im]
 
 		anim = animation.FuncAnimation(fig, animate, init_func = init, frames = n,
@@ -57,12 +68,16 @@ def example():
 
 	bounds = [-3., 3., -2., 2.]
 
-	delta = 0.025
+	delta = 0.05
 	x = np.arange(bounds[0], bounds[1], delta)
 	y = np.arange(bounds[2], bounds[3], delta)
 	X, Y = np.meshgrid(x, y)
 	Z = 10.+ 10.*mlab.bivariate_normal(X, Y, 1.0, 1.0, 0.0, 0.0)
 	poisson = 0.05*np.sqrt(np.abs(Z))
 
-	#uncertainty_plot(np.ones(4).reshape((2,2)), 0.1*np.ones(4).reshape((2,2)))
-	uncertainty_plot(Z, poisson, extent = bounds, barlabel = 'label', title = 'Title')
+	mask = np.zeros(Z.shape)
+	mask[-25:, -25:] = True
+
+	uncertainty_plot(Z, poisson, mask, extent = bounds, barlabel = 'label', title = 'Title')
+
+example()
