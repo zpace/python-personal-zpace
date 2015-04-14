53001a413cf2cd824a6c4b0632f9cc0a24f67378
zpace <zpace21@gmail.com>
2015-04-14 11:37:49 -0500

    added sloan donwload tool

---
+++ gz2tools.py
@@ -0,0 +1,24 @@
+def download_sloan_im(ra, dec, scale, width = 256, height = 256):
+	'''
+	ra & dec: sky coords
+	scale: pixel scale (in arcsec) of image (.02 * rPetro is recommended)
+	'''
+	import numpy as np
+	import matplotlib.pyplot as plt
+	import matplotlib.image as mpimg
+	import urllib
+	import io
+
+	plt.ioff()
+
+	base_url = 'http://skyservice.pha.jhu.edu/DR10/ImgCutout/getjpeg.aspx?opt=0'
+	im_url = base_url + '&width=' + str(width) + '&height=' + str(height) + '&scale=' + str(np.round(scale, 3)) + '&ra=' + str(ra) + '&dec=' + str(dec)
+
+	im = urllib.urlopen(im_url)
+	image_file = io.BytesIO(im.read())
+	img = mpimg.imread(image_file, format = 'jpg')
+	plt.imshow(img, extent = [-width*scale/2., width*scale/2., -height*scale/2., height*scale/2.])
+	plt.show()
+
+def example():
+	download_sloan_im(197.61446, 18.43817 , 0.02 * 18.23)
