947acfb71a8cc6bf65b153952d830e6361609697
Zach Pace <zpace21@gmail.com>
2016-06-21 01:33:40 -0500

    add spec_tools with spectral index tools

---
+++ spec_tools.py
@@ -0,0 +1,42 @@
+def D4000_index(l, s):
+    '''
+    compute D4000 index
+
+    params:
+     - l: wavelength array [Angstroms], length n
+     - s: spectrum [flux or flux density units], length n
+    '''
+
+    blue = [3850., 3950.]
+    red = [4100., 4200.]
+
+    blueflux = (s[(l > blue[0]) * (l < blue[-1])]).sum()
+    redflux = (s[(l > blue[0]) * (l < blue[-1])]).sum()
+
+    return redflux / blueflux
+
+def HdA_index(l, s):
+    '''
+    compute HdA index
+
+    params:
+     - l: wavelength array [Angstroms], length n
+     - s: spectrum [flux or flux density units], length n
+    '''
+
+    blue = [4041.600, 4079.750]
+    red = [4128.500, 4161.000]
+    line = [4083.500, 4122.250]
+
+    assert len(l_edges) == len(s) + 1, 'invalid length of l_edges'
+
+    lred = l[(l > red[0]) * (l < red[-1])]
+    lblue = l[(l > blue[0]) * (l < blue[-1])]
+    lline = l[(l > line[0]) * (l < line[-1])]
+    dl = np.mean(lline[1:] - lline[:-1])
+    F_c = np.append(
+            s[(l > red[0]) * (l < red[-1])],
+            s[(l > blue[0]) * (l < blue[-1])]).mean()
+
+    EW = (1. - s[(l > line[0]) * (l < line[-1])]/F_c) * dl
+    return EW
