376970069e5851a3f224fef8e5ef40c62de73ceb
zpace <zpace21@gmail.com>
2016-10-10 16:43:05 -0500

    Hdelta and D4000 functions (vectorized)

--- spec_tools.py
+++ spec_tools.py
@@ -17,38 +17,51 @@ def D4000_index(l, s):
     '''
 
     blue = [3850., 3950.]
-    red = [4100., 4200.]
+    red = [4000., 4100.]
 
-    blueflux = (s[(l > blue[0]) * (l < blue[-1])]).sum()
-    redflux = (s[(l > red[0]) * (l < red[-1])]).sum()
+    bluef = (s[(l > blue[0]) * (l < blue[-1]), ...]).mean(axis=0)
+    redf = (s[(l > red[0]) * (l < red[-1]), ...]).mean(axis=0)
 
-    return redflux / blueflux
+    return redf / bluef
 
 
-def HdA_index(l, s):
+def Hdelta_A_index(l, s, dl=None):
     '''
     compute HdA index
+
+    define index wrt blue continuum and red continuum
 
     params:
      - l: wavelength array [Angstroms], length n
      - s: spectrum [flux or flux density units], length n
     '''
 
+    if dl is None:
+        dl = determine_dl(l)
+
     blue = [4041.600, 4079.750]
     red = [4128.500, 4161.000]
     line = [4083.500, 4122.250]
 
-    assert len(l_edges) == len(s) + 1, 'invalid length of l_edges'
+    lred_m = (l > red[0]) * (l < red[-1])
+    lblue_m = (l > blue[0]) * (l < blue[-1])
+    lline_m = (l > line[0]) * (l < line[-1])
 
-    lred = l[(l > red[0]) * (l < red[-1])]
-    lblue = l[(l > blue[0]) * (l < blue[-1])]
-    lline = l[(l > line[0]) * (l < line[-1])]
-    dl = np.mean(lline[1:] - lline[:-1])
-    F_c = np.append(
-            s[(l > red[0]) * (l < red[-1])],
-            s[(l > blue[0]) * (l < blue[-1])]).mean()
+    lred = l[lred_m]
+    lblue = l[lblue_m]
+    lline = l[lline_m]
 
-    EW = (1. - s[(l > line[0]) * (l < line[-1])] / F_c) * dl
+    sline = s[lline_m, ...]
+
+    # calculate expected continuum flux within line
+    red_avg = s[lred_m, ...].mean(axis=0)
+    blue_avg = s[lblue_m, ...].mean(axis=0)
+    cont_slope = (red_avg - blue_avg) / (red[0] - blue[-1])
+    cont_interp = lambda l: cont_slope * (l - blue[0]) + blue_avg
+    # use trapezoidal rule to approximate deficit in continuum flux-density
+    EW = np.trapz(x=l[lline_m], y=(1. - (sline / cont_interp(lline))),
+                  axis=0)
+
     return EW
 
 
@@ -76,3 +89,21 @@ def shift_to_rest(logl, v):
 def determine_dlogl(logl):
     dlogl = np.round(np.mean(logl[1:] - logl[:-1]), 8)
     return dlogl
+
+def determine_dl(l):
+    logl = np.log10(l)
+    dlogl = determine_dlogl(logl)
+    llllim, llulim = logl - dlogl/2., logl + dlogl/2.
+    lllim, lulim = np.log10(llllim), np.log10(llulim)
+    dl = lulim - lllim
+    return dl
+
+def cube2rss(a, axis=0):
+    '''
+    transform cube-shaped data into RSS-shaped data
+    '''
+    if type(a) is not list:
+        a = [a, ]
+
+    a = np.row_stack([a_.reshape((-1, ) + (a_.shape[axis], )).T for a_ in a])
+    return a
